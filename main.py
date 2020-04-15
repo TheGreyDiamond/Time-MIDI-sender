552799d21987b7f555b2b5ccc3fe00dfd08a9cc7
Soeren Oesterwind <soeren.oesterwind@hotmail.de>
2020-04-15 22:36:47 +0200

    first GUI impement

--- main.py
+++ main.py
@@ -2,74 +2,113 @@ from __future__ import print_function
 import pygame
 import time
 import mido
-
 from mido import Message
+import tkinter
+import easygui
 pygame.mixer.init()
 
 print("Welcome")
 isOkay = False
 configY = None
+guiEn = False
 #markerObj = None
-while not isOkay:
-    conf = input("Do you want to load a config file (Y/N) => ")
-    if(conf == "Y"):
-        configF = input("Configfile name => ")
-        try:
-            co = open(configF, "r")
-            confLines = co.readlines()
-            co.close()
-        except FileNotFoundError:
-            print("Unable to load config file. Retry.")
+notSaved = True
+def exitProg():
+    if(notSaved):
+        ot = easygui.buttonbox('There are unsaved changes', 'Quit', ('Save', 'Cancle', 'Quit'))
+        if(ot == "Save"):
+            print("Saving whats that?")
+        elif(ot == "Cancle"):
+            print("Canceling")
         else:
-            configY = True
-            isOkay = True
-    elif(conf == "N"):
-        print("Okay, not loading a config file")
-        configY = False
-        isOkay = True
+            print("Quiting")
+            exit()
     else:
-        print("Invalid answer")
-if(configY):
-    confAtr = {}
-    for c in confLines:
-        if(not c.startswith("#")):
-            c = c.replace("\n","")
-            cp = c.split(":")
-            confAtr[cp[0]] = cp[1]
-    portname = confAtr["port"]
-    musicFile = confAtr["music"]
-    markerFile = confAtr["marker"]
-    pygame.mixer.music.load(musicFile)
-else:
-    portname = input("Port => ")
-    
-        
-        #pygame.mixer.music.load(musicFile)
-        
-    #portname = "testLoopback 2"
+        exit()
+
+while not isOkay:
+    qu = input("Do you want to use GUI (Y/N) WARNING GUI IS UNSTABLE => ")
+    if(qu == "Y"):
+        isOkay = True
+        guiEn = True
+    else:
+        print("Not using GUI.")
+        isOkay = True
+        guiEn = False
+if(guiEn == False):  
     isOkay = False
     while not isOkay:
-        try:
-            musicFile = input("Music file=> ")
-            pygame.mixer.music.load(musicFile)
+        conf = input("Do you want to load a config file (Y/N) => ")
+        if(conf == "Y"):
+            configF = input("Configfile name => ")
+            try:
+                co = open(configF, "r")
+                confLines = co.readlines()
+                co.close()
+            except FileNotFoundError:
+                print("Unable to load config file. Retry.")
+            else:
+                configY = True
+                isOkay = True
+        elif(conf == "N"):
+            print("Okay, not loading a config file")
+            configY = False
             isOkay = True
-        except pygame.error:
-            print("Unable to open music file. Retry.")
+        else:
+            print("Invalid answer")
+    if(configY):
+        confAtr = {}
+        for c in confLines:
+            if(not c.startswith("#")):
+                c = c.replace("\n","")
+                cp = c.split(":")
+                confAtr[cp[0]] = cp[1]
+        portname = confAtr["port"]
+        musicFile = confAtr["music"]
+        markerFile = confAtr["marker"]
+        pygame.mixer.music.load(musicFile)
+    else:
+        portname = input("Port => ")
+        
             
-    isOkay = False
-    while not isOkay:
-        try:
-            markerFile = input("Marker file=> ")
-            markerObj = open(markerFile, "r")
-            markerObj.close()
-            isOkay = True
-        except FileNotFoundError:
-            print("Unable to open marker file. Retry.")
-    #markerFile = input("Marker file=> ")
-    #if(markerFile == "m"):
-    #    f = open("points.dat","r")
-    #else:
-    #    f = open(markerFile, "r")
+            #pygame.mixer.music.load(musicFile)
+            
+        #portname = "testLoopback 2"
+        isOkay = False
+        while not isOkay:
+            try:
+                musicFile = input("Music file=> ")
+                pygame.mixer.music.load(musicFile)
+                isOkay = True
+            except pygame.error:
+                print("Unable to open music file. Retry.")
+                
+        isOkay = False
+        while not isOkay:
+            try:
+                markerFile = input("Marker file=> ")
+                markerObj = open(markerFile, "r")
+                markerObj.close()
+                isOkay = True
+            except FileNotFoundError:
+                print("Unable to open marker file. Retry.")
+
+else:
+    print("Do GUI stuff here")
+    window = tkinter.Tk()
+    window.title("Time MIDI Sender")
+    window.geometry('800x300')
+    window["bg"] = "white"
+    menu = tkinter.Menu(window)
+    window.config(menu=menu)
+    file = tkinter.Menu(menu)
+    file.add_command(label="Exit", command=exitProg)
+    menu.add_cascade(label="File", menu=file)
+    edit = tkinter.Menu(menu)
+    edit.add_command(label="Undo")
+    menu.add_cascade(label="Edit", menu=edit)
+    markerFile = "points.dat" ### JUST FOR DEBUG
+
 markerObj = open(markerFile, "r")
 raw = markerObj.readlines()
 markerObj.close()
@@ -106,4 +145,5 @@ try:
             
 except KeyboardInterrupt:
     pygame.mixer.music.stop()
+    exitProg()
     print(points)
