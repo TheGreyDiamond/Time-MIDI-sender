82936193df2f8cba1989635369640a790fe01e42
Soeren Oesterwind <soeren.oesterwind@hotmail.de>
2020-04-16 17:29:15 +0200

    added Playcontrol GUI

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import mido
 from mido import Message
 import tkinter
 from tkinter import filedialog
+from tkinter.messagebox import showinfo
 import easygui
 pygame.mixer.init()
+
+version = "1.0.6"
 
 print("Welcome")
 isOkay = False
@@ -14,15 +17,28 @@ configY = None
 guiEn = False
 #markerObj = None
 notSaved = True
+loaded = False
+portObj = None
+playing = False
+paused = False
+projectName = "Project: ---"
+opendFile = None
+points = {}
 def exitProg():
     if(notSaved):
         ot = easygui.buttonbox('There are unsaved changes', 'Quit', ('Save', 'Cancle', 'Quit'))
         if(ot == "Save"):
-            print("Saving whats that?")
+            saveConfig()
+
+            # When saveing is done
+            if(play == True or loaded == True):
+                pygame.mixer.music.stop()
         elif(ot == "Cancle"):
             print("Canceling")
         else:
             print("Quiting")
+            if(play == True or loaded == True):
+                pygame.mixer.music.stop()
             exit()
     else:
         exit()
@@ -31,8 +47,19 @@ def openConfig():
     global portname
     global musicFile
     global markerFile
-    path = filedialog.askopenfilename(initialdir = "/",title = "Select config file",filetypes = (("Config","*.conf"),("all files","*.*")))
-    co = open(path, "r")
+    global projectName
+    global opendFile
+    file = filedialog.askopenfilename(initialdir = "/",title = "Select config file",filetypes = (("Config","*.conf"),("all files","*.*")))
+    print(file)
+    co = open(file, "r")
+    rawpath = file.split("/")
+    projectName = "Project: " + str(rawpath[len(rawpath)-1])[0:len(str(rawpath[len(rawpath)-1]))-5]
+    opendFile["text"] = "Project: " + str(rawpath[len(rawpath)-1])[0:len(str(rawpath[len(rawpath)-1]))-5]
+    path = ""
+    for p in rawpath:
+        if(not p.endswith(".conf")):
+           path += p + "/"
+    print(path)
     confLines = co.readlines()
     co.close()
     confAtr = {}
@@ -42,22 +69,101 @@ def openConfig():
             cp = c.split(":")
             confAtr[cp[0]] = cp[1]
     portname = confAtr["port"]
-    musicFile = confAtr["music"]
-    markerFile = confAtr["marker"]
-    pygame.mixer.music.load(musicFile)    
-    
+    rawmusicFile = confAtr["music"]
+    musicFile = path + rawmusicFile
+    rawmarkerFile = confAtr["marker"]
+    markerFile = path + rawmarkerFile
+     
+
+
+def load():
+    global musicFile
+    global markerFile
+    global portObj
+    global portname
+    global loaded
+    global points
+    if( not loaded):
+        pygame.mixer.music.load(musicFile)
+        markerObj = open(markerFile, "r")
+        raw = markerObj.readlines()
+        markerObj.close()
+        points = {}
+        for o in raw:       # convert to dict
+            if(not o.startswith("#")):
+                p = o.split(":")
+                points[int(p[0])] = float(p[1])
+        print(points)
+        loaded = True
+        portObj = mido.open_output(portname, autoreset=True)
+    else:
+        showinfo("Info", "Project is already loaded!")
+
+def unload():
+    global musicFile
+    global markerFile
+    global portObj
+    global portname
+    global loaded
+    global paused
+    if(loaded == True):
+        musicFile = ""
+        markerFile = ""
+        portObj = None
+        portname = ""
+        pygame.mixer.music.unload()
+        loaded = False
+        paused = False
+    else:
+        showinfo("Info", "There is no loaded project")
+
+def myPlay():
+    global paused
+    global playing
+    playing = True
+    if(not paused):
+        pygame.mixer.music.play()
+        print("playing")
+    else:
+        print("unpausing")
+        pygame.mixer.music.unpause()
+        paused = False
+
+def myPause():
+    global paused
+    global playing
+    playing = False
+    print("Pasuing")
+    pygame.mixer.music.pause()
+    paused = True
+
+def myStop():
+    global playing
+    playing = False
+    pygame.mixer.music.stop()
+    paused= False
 def saveConfig():
     print("save conf")
 
-while not isOkay:
-    qu = input("Do you want to use GUI (Y/N) WARNING GUI IS UNSTABLE => ")
-    if(qu == "Y"):
-        isOkay = True
-        guiEn = True
-    else:
-        print("Not using GUI.")
-        isOkay = True
-        guiEn = False
+def aboutPopup():
+    message  = '''Thank you for using Time MIDI sender!\nUsing version: ''' + version + '''\nAuthors: TheGreydiamond(thgreydiamond.de)'''
+    showinfo("About", message)
+
+## Will force usage of GUI since 1.0.6
+guiEn = True
+    
+#while not isOkay:
+#    qu = input("Do you want to use GUI (Y/N) WARNING GUI IS UNSTABLE => ")
+#    if(qu == "Y"):
+#        isOkay = True
+#        guiEn = True
+#    else:
+#        print("Not using GUI.")
+#        isOkay = True
+#        guiEn = False
+
+
+        
 if(guiEn == False):  
     isOkay = False
     while not isOkay:
@@ -115,6 +221,8 @@ if(guiEn == False):
                 isOkay = True
             except FileNotFoundError:
                 print("Unable to open marker file. Retry.")
+    load()
+    pygame.mixer.music.play()
 
 else:
     print("Do GUI stuff here")
@@ -129,47 +237,91 @@ else:
     file.add_command(label="Save", command=saveConfig)
     file.add_command(label="Exit", command=exitProg)
     
+    opendFile = tkinter.Label(window, text = projectName)
+    opendFile.grid(row=0, column=0)
+    loadB = tkinter.Button(window, text="Load", command = load)
+    loadB.grid(row=2, column = 1, padx=2)
+    unloadB = tkinter.Button(window, text="Unload", command = unload)
+    unloadB.grid(row=2, column = 2, padx=2)
+
+    playControls = tkinter.Frame(window, borderwidth = 1,width=100, height=100, bg="White", relief=tkinter.SUNKEN)
+    control = tkinter.Label(playControls, text = "Play control")
+    control.grid(row=1, column = 5)
+    play = tkinter.Button(playControls, text="Play", command = myPlay)
+    play.grid(row=2, column = 1, padx = 2)
+    pause = tkinter.Button(playControls, text="Pause", command = myPause)
+    pause.grid(row=2, column = 2, padx = 2)
+    stop = tkinter.Button(playControls, text="Stop", command = myStop)
+    stop.grid(row=2, column = 3, padx = 2)
+    playControls.grid(row=10, column=0, rowspan=5, columnspan = 1)
+    
+    
+    
     menu.add_cascade(label="File", menu=file)
     edit = tkinter.Menu(menu)
     #edit.add_command(label="Undo")
     menu.add_cascade(label="Edit", menu=edit)
-    markerFile = "points.dat" ### JUST FOR DEBUG
+    helpM = tkinter.Menu(menu)
+    helpM.add_command(label="About", command=aboutPopup)
+    menu.add_cascade(label="Help", menu=helpM)
+    window.update()
+    
+    window.update()
+    #markerFile = "points.dat" ### JUST FOR DEBUG
 
-markerObj = open(markerFile, "r")
-raw = markerObj.readlines()
-markerObj.close()
-points = {}
-for o in raw:       # convert to dict
-    if(not o.startswith("#")):
-        p = o.split(":")
-        points[int(p[0])] = float(p[1])
-print(points)
-pygame.mixer.music.play()
 
-try:
-    with mido.open_output(portname, autoreset=True) as port:
-        print(port)
-        while True:
-            #print("Time: " + str(pygame.mixer.music.get_pos()/1000) + " Seconds")
-            
-            
 
-                #print(" !!!!!!!!!!! " + str(str(points[pygame.mixer.music.get_pos()]).endswith(".0")))
-            ti = pygame.mixer.music.get_pos()
-            if( ti in points):
-                if(str(points[ti]).endswith(".0") == True):
-                    print("Note on")
-                    msg = Message('note_on', note=int(str(points[ti]).split(".")[0]), channel = 1, velocity = 60)
-                    print('Sending {}'.format(msg))
-                    port.send(msg)
-                else:
-                    print("Note off")
-                    msg = Message('note_off', note=int(str(points[ti]).split(".")[0]), channel = 1)
-                    print('Sending {}'.format(msg))
-                    port.send(msg)
 
-            
-except KeyboardInterrupt:
-    pygame.mixer.music.stop()
-    exitProg()
-    print(points)
+if(guiEn == False):
+    try:
+        with mido.open_output(portname, autoreset=True) as port:
+            print(port)
+            while True:
+                #print("Time: " + str(pygame.mixer.music.get_pos()/1000) + " Seconds")
+                
+                
+
+                    #print(" !!!!!!!!!!! " + str(str(points[pygame.mixer.music.get_pos()]).endswith(".0")))
+                ti = pygame.mixer.music.get_pos()
+                if( ti in points):
+                    if(str(points[ti]).endswith(".0") == True):
+                        print("Note on")
+                        msg = Message('note_on', note=int(str(points[ti]).split(".")[0]), channel = 1, velocity = 60)
+                        print('Sending {}'.format(msg))
+                        port.send(msg)
+                    else:
+                        print("Note off")
+                        msg = Message('note_off', note=int(str(points[ti]).split(".")[0]), channel = 1)
+                        print('Sending {}'.format(msg))
+                        port.send(msg)
+
+                
+    except KeyboardInterrupt:
+        pygame.mixer.music.stop()
+        exitProg()
+        print(points)
+else:
+    
+    while True:
+        window.update()
+        if(loaded):
+            #print(portObj)
+            evt = pygame.mixer.music.get_endevent()
+            if(evt != 0): print(evt)
+            if(playing):
+                ti = pygame.mixer.music.get_pos()
+                if( ti in points):
+                    if(str(points[ti]).endswith(".0") == True):
+                        print("Note on")
+                        msg = Message('note_on', note=int(str(points[ti]).split(".")[0]), channel = 1, velocity = 60)
+                        print('Sending {}'.format(msg))
+                        portObj.send(msg)
+                    else:
+                        print("Note off")
+                        msg = Message('note_off', note=int(str(points[ti]).split(".")[0]), channel = 1)
+                        print('Sending {}'.format(msg))
+                        portObj.send(msg)
+            #if(play):
+            #    pygame.mixer.music.play()
+            #else:
+            #    pygame.mixer.music.pause()
